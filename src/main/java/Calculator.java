7911c91d9f1f56619c7dd786e48e77f089799dca
Joung Hyeon Seop <21600665@handong.edu>
2018-05-09 21:44:40 +0900

    모든 기능 완료

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator {
+
+    public int add(int l, int r) {
+        return l + r;
+    }
+    public int sub(int l, int r) {
+        return l - r;
+    }
+    public int mul(int l, int r) {
+        return l * r;
+    }
+    public int div(int l, int r) throws ArithmeticException {
+        return l / r;
+    }
+
+}
